645d67b1959955e81f44c12638c7e1934f59a3af
Satyam <satyam@satyam.com.ar>
2017-12-17 17:33:59 +0100

    Added routing

--- packages/desktop/src/components/App/index.jsx
+++ packages/desktop/src/components/App/index.jsx
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { compose } from 'recompose';
 
-import LoginForm from '../LoginForm';
+import loadModule from '../utils/moduleLoader';
+import { selUsuarioActivo } from '../../store/selectors';
 
-export default class App extends Component {
+export class App extends Component {
+  checkLoggedIn = () => {
+    const { usuario, history, location } = this.props;
+    if (location.pathname !== '/login' && !usuario.idUsuario) {
+      history.push('/login');
+    }
+  };
+  componentDidMount = () => {
+    this.checkLoggedIn();
+  };
+  componentDidUpdate = () => {
+    this.checkLoggedIn();
+  };
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <title>Sistrac</title>
+        </Helmet>
         <Navbar staticTop>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Sistrac</a>
+              <a href="/">Sistrac</a>
             </Navbar.Brand>
           </Navbar.Header>
           <Nav>
-            <NavItem eventKey={1} href="#">
-              Link
+            <NavItem eventKey={1} href="/estaciones">
+              Estaciones
             </NavItem>
             <NavItem eventKey={2} href="#">
               Link
@@ -29,8 +49,23 @@ export default class App extends Component {
             </NavDropdown>
           </Nav>
         </Navbar>
-        <LoginForm />
+        <Route
+          path="/estaciones"
+          component={loadModule(() =>
+            import(/* webpackChunkName: "estaciones" */ '../Estaciones'),
+          )}
+        />
+        <Route
+          path="/login"
+          component={loadModule(() =>
+            import(/* webpackChunkName: "login" */ '../LoginForm'),
+          )}
+        />
       </div>
     );
   }
 }
+
+export const mapStateToProps = state => ({ usuario: selUsuarioActivo(state) });
+
+export default compose(withRouter, connect(mapStateToProps))(App);
