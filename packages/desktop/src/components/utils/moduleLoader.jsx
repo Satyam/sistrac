036e72f6b28ba2355d0c8660312aec2ba6274469
Satyam <satyam@satyam.com.ar>
2017-12-17 19:40:27 +0100

    Fixed passing props to dynamically loaded module

--- packages/desktop/src/components/utils/moduleLoader.jsx
+++ packages/desktop/src/components/utils/moduleLoader.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default function loadModule(loader, props) {
+export default function loadModule(loader) {
   return class ModuleLoader extends Component {
     constructor(...args) {
       super(...args);
@@ -13,7 +13,7 @@ export default function loadModule(loader, props) {
     componentWillUnmount() {
       this.mounted = false;
     }
-    setComponent = (module) => {
+    setComponent = module => {
       if (this.mounted) {
         /* eslint-disable no-underscore-dangle */
         this.setState({ Component: module.default || module });
@@ -22,7 +22,11 @@ export default function loadModule(loader, props) {
     };
     render() {
       const C = this.state.Component;
-      return C ? <C {...props} /> : <img alt="loading" src="/icons/loading.gif" />;
+      return C ? (
+        <C {...this.props} />
+      ) : (
+        <img alt="loading" src="/icons/loading.gif" />
+      );
     }
   };
 }
