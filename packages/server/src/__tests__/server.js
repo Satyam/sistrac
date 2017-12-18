480ceaedc73cf37746eb1ea71b6a16bf2f8f4bbe
Satyam <satyam@satyam.com.ar>
2017-12-18 19:05:12 +0100

    finished a pending test

--- packages/server/src/__tests__/server.js
+++ packages/server/src/__tests__/server.js
@@ -15,17 +15,33 @@ describe('server integration test', () => {
     expect(req.path).toBe('/');
   });
   it('login', async () => {
-    const resp = await fetch(`${REST_HOST}:${REST_PORT}${REST_API_PATH}/usuarios/login`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        usuario: 'satyam',
-        password: 'uma123',
-      }),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+    const resp = await fetch(
+      `${REST_HOST}:${REST_PORT}${REST_API_PATH}/usuarios/login`,
+      {
+        method: 'PUT',
+        body: JSON.stringify({
+          usuario: 'satyam',
+          password: 'uma123',
+        }),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
       },
+    );
+    expect(resp.status).toBe(200);
+    const reply = await resp.text();
+    const json = JSON.parse(reply);
+    expect(json).toEqual({
+      idUsuario: 10,
+      usuario: 'satyam',
+      nivel: 1,
+      rolGuarda: true,
+      rolDios: true,
+      rolSupervisor: true,
+      rolMecanico: true,
+      funcion: 0,
+      nombre: 'Daniel Barreiro',
     });
-    console.log(resp.status, await resp.text());
   });
 });
