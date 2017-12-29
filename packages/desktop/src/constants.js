6dfddd2eefc2e1fce6d69b28ac4ab895134db490
Satyam <satyam@satyam.com.ar>
2017-12-29 18:52:08 +0100

    deleted file wrongly copied to wrong folder

--- packages/desktop/src/constants.js
+++
@@ -1,9 +0,0 @@
-export const NAME = 'estaciones';
-
-export const GET_ESTACIONES = `${NAME} / get estaciones`;
-
-export const GET_ESTACION = `${NAME} / get estacion`;
-export const UPDATE_ESTACION = `${NAME} / update estacion`;
-export const CREATE_ESTACION = `${NAME} / create estacion`;
-export const DELETE_ESTACION = `${NAME} / delete estacion`;
-
