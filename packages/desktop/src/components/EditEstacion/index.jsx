7a1a72a753f4265c0725bba95555f75465f29b04
Satyam <satyam@satyam.com.ar>
2018-01-31 19:36:29 +0100

    got rid of react-bootstrap

--- packages/desktop/src/components/EditEstacion/index.jsx
+++ packages/desktop/src/components/EditEstacion/index.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
-import {
-  Form,
-  FormGroup,
-  Col,
-  FormControl,
-  ControlLabel,
-  Button,
-  HelpBlock,
-  Label,
-  Glyphicon,
-} from 'react-bootstrap';
+import { Button } from '@devasatyam/controls/lib/Button';
+import Form from '@devasatyam/controls/lib/Form';
+import { Grid, Row, Col } from '@devasatyam/controls/lib/Grid';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import formatcoords from 'formatcoords';
 import isPlainClick from '_components/utils/isPlainClick';
@@ -110,53 +102,40 @@ export default class EditEstacion extends Component {
     const { idEstacion, nombre, latitud, longitud, duplicado } = this.state;
     const position = [parseFloat(latitud), parseFloat(longitud)];
     return (
-      <Form horizontal>
-        <FormGroup
-          controlId="idEstacion"
-          validationState={this.validateIdEstacion()}
+      <Form>
+        <Form.Field
+          type="text"
+          placeholder="Sigla"
+          value={idEstacion}
+          onChange={this.idEstacionChange}
         >
-          <Col componentClass={ControlLabel} mdOffset={2} md={2} xs={3}>
-            Sigla
-          </Col>
-          <Col md={6} xs={9}>
-            <FormControl
-              type="text"
-              placeholder="Sigla"
-              value={idEstacion}
-              onChange={this.idEstacionChange}
-            />
-            <FormControl.Feedback />
-            {duplicado && <HelpBlock>duplicado</HelpBlock>}
-          </Col>
-        </FormGroup>
+          <Form.Label>Sigla</Form.Label>
+          <Form.Help>duplicado={duplicado}</Form.Help>
+        </Form.Field>
 
-        <FormGroup controlId="nombre" validationState={this.validateNombre()}>
-          <Col componentClass={ControlLabel} mdOffset={2} md={2} xs={3}>
-            Nombre
-          </Col>
-          <Col md={6} xs={9}>
-            <FormControl
-              type="text"
-              placeholder="Nombre Completo"
-              value={nombre}
-              onChange={this.nombreChange}
-            />
-            <FormControl.Feedback />
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col mdOffset={2} md={8} xs={12}>
-            <Map center={position} zoom={13} onclick={this.mapClick}>
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-              />
-              <Marker position={position} />
-            </Map>
-          </Col>
-        </FormGroup>
-        <FormGroup className="coords">
-          <Label>
+        <Form.Field
+          type="text"
+          placeholder="Nombre Completo"
+          value={nombre}
+          onChange={this.nombreChange}
+        >
+          <Form.Label>Nombre</Form.Label>
+        </Form.Field>
+        <Grid>
+          <Row>
+            <Col mdOffset={2} md={8} xs={12}>
+              <Map center={position} zoom={13} onclick={this.mapClick}>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                <Marker position={position} />
+              </Map>
+            </Col>
+          </Row>
+        </Grid>
+        <Form.Field className="coords">
+          <Form.Label>
             {formatcoords(parseFloat(latitud), parseFloat(longitud)).format(
               'FFf',
               {
@@ -164,58 +143,35 @@ export default class EditEstacion extends Component {
                 decimalPlaces: 2,
               },
             )}
-          </Label>
-        </FormGroup>
-        <FormGroup controlId="latitud" validationState={this.validateLatitud()}>
-          <Col componentClass={ControlLabel} mdOffset={2} md={2} xs={3}>
-            Latitud
-          </Col>
-          <Col md={6} xs={9}>
-            <FormControl
-              type="text"
-              placeholder="Latitud"
-              value={latitud}
-              onChange={this.latitudChange}
-            />
-            <FormControl.Feedback />
-          </Col>
-        </FormGroup>
-
-        <FormGroup
-          controlId="longitud"
-          validationState={this.validateLongitud()}
+          </Form.Label>
+        </Form.Field>
+        <Form.Field
+          type="text"
+          placeholder="Latitud"
+          value={latitud}
+          onChange={this.latitudChange}
         >
-          <Col componentClass={ControlLabel} mdOffset={2} md={2} xs={3}>
-            Longitud
-          </Col>
-          <Col md={6} xs={9}>
-            <FormControl
-              type="text"
-              placeholder="Longitud"
-              value={longitud}
-              onChange={this.longitudChange}
-            />
-            <FormControl.Feedback />
-          </Col>
-        </FormGroup>
+          <Form.Label>Latitud</Form.Label>
+        </Form.Field>
 
-        <FormGroup>
-          <Col mdOffset={4} md={6} xsOffset={3} xs={9}>
-            <Button
-              type="submit"
-              onClick={this.formSubmit}
-              disabled={!this.validates()}
-            >
-              Agregar
-            </Button>{' '}
-            <div className="btn btn-success">
-              <Glyphicon glyph="ok" />
-            </div>{' '}
-            <div className="btn btn-danger">
-              <Glyphicon glyph="remove" />
-            </div>
-          </Col>
-        </FormGroup>
+        <Form.Field
+          type="text"
+          placeholder="Longitud"
+          value={longitud}
+          onChange={this.longitudChange}
+        >
+          <Form.Label>Longitud</Form.Label>
+        </Form.Field>
+
+        <Form.Field type="buttonGroup">
+          <Button
+            type="submit"
+            onClick={this.formSubmit}
+            disabled={!this.validates()}
+          >
+            Agregar
+          </Button>
+        </Form.Field>
       </Form>
     );
   }
